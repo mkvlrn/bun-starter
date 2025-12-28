8042213e4553298d14fa56fb7cf24d2e4f981c19
Mike Valeriano <mkvlrn@gmail.com>
2025-12-28 02:54:44 -0300

    chore: bump deps

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 /** biome-ignore-all lint/suspicious/noConsole: needed, yo */
-/** biome-ignore-all lint/correctness/noNodejsModules: backend */
 
-import process from "node:process";
-import { divide } from "#/math/advanced";
-import { add } from "#/math/basic";
+import { divide } from "#math/advanced";
+import { add } from "#math/basic";
 
 const sum = add(1, 2);
 console.log(`1 + 2 = ${sum}`);
 const div = divide(4, 2);
 if (div.isError) {
   console.error(div.error.message);
-  process.exit(1);
+} else {
+  console.log(`4 / 2 = ${div.value}`);
 }
-console.log(`4 / 2 = ${div.value}`);
